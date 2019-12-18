908b72dc64ff3e953403254e7f84cd1da0fa3a2d
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2019-12-17 16:12:28 -0800

    Please Lord

---
+++ Continyoom/ui/pause_menu/PauseScene.gd
@@ -0,0 +1,41 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		#de presses action, only important when action is triggered in code
+		Input.action_release("pause")
+		
+		#show/hide when necessary
+		if get_tree().paused:
+			self.hide()
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		if not get_tree().paused:
+			self.show()
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			if not Input.get_connected_joypads().empty():
+				$resume.grab_focus()
+		
+		#switch pause state
+		get_tree().paused = not get_tree().paused
+
+
+func _on_Resume_pressed():
+	Input.action_press("pause")
+
+
+func _on_Menu_pressed():
+	get_tree().paused = false
+	self.hide()
+	get_tree().change_scene("ui/start_menu/StartMenu.tscn")
+	get_node("..").queue_free()
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
