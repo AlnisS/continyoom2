dd0af33f367d4f6a0e397c8b01ce52d18e592c2d
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2020-01-16 09:30:34 -0800

    Stopwatch added

---
+++ Continyoom/ui/time_keeper/TimeKeeper.gd
@@ -0,0 +1,19 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var time = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("reset"):
+		time = 0.0
+	
+	time += delta
+	$Label.text = "Time: " + str(stepify(time, 0.01))
